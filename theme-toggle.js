0385b2ea3f342e97422a8d9dd254c3cd4b9b9f2d
Nadith Premaratne <nadmalinda@gmail.com>
2025-06-29 21:06:43 +0800

    Create theme-toggle.js

---
+++ theme-toggle.js
@@ -0,0 +1,8 @@
+<script>
+  document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('toggle-theme');
+    button.addEventListener('click', () => {
+      document.body.classList.toggle('dark');
+    });
+  });
+</script>
