ecdedf8a70a8d7d44853c2d27b62e62d96dcac4d
Nadith Premaratne <nadmalinda@gmail.com>
2025-06-29 21:17:35 +0800

    Update theme-toggle.js

--- theme-toggle.js
+++ theme-toggle.js
@@ -1,8 +1,12 @@
 <script>
   document.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById('toggle-theme');
+    const currentMode = localStorage.getItem('theme');
+    if (currentMode === 'dark') document.body.classList.add('dark');
+
     button.addEventListener('click', () => {
       document.body.classList.toggle('dark');
+      localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
     });
   });
 </script>
